b91b9a54c86e3d30b2cd3be5e0cf28593c334ecc
jayong128 <novo200297@gmail.com>
2021-05-29 20:31:44 +0900

    test

---
+++ buffer.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+int main(){
+    putchar('e');
+    sleep(1);
+    fputc('H', stderr);
+
+    putchar('h');
+    sleep(1);
+    fputc('E', stderr);
+ 
+    putchar('\n');
+    sleep(1);
+    fputc('L', stderr);
+ 
+    putchar('p');
+    sleep(1);
+    fputc('L', stderr);
+ 
+    putchar('u');
+    sleep(1);
+    fputc('O', stderr);
+ 
+    putchar('b');
+    sleep(1);
+    fputc('!', stderr);
+ 
+    putchar('\n');
+    sleep(1);
+    fputc('\n', stderr);
+ 
+    return 0;
+}
